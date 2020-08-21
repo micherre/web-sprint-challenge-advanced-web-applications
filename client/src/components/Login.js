2465cb0705569b70482f0442849b763534fab5f9
Micherre Fox <micherrefox@gmail.com>
2020-08-21 12:17:06 -0400

    started login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { axiosWithAuth } from './axiosWithAuth'
+import { useHistory } from 'react-router-dom'
 
-const Login = () => {
+export function Login(props) {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <form onSubmit={handleSubmit}>
+        <input
+          onChange={(e) => setForm({ ...form, username: e.target.value})}
+        />
+        <input
+          onChange={(e) => setForm({ ...form, password: e.target.value})}
+          />
+        <button type='submit'> Login </button>
+      </form>
     </>
   );
 };
